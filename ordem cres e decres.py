cadc8688ed5100f220ca4ed6a0479c4227c63418
1-LEI-DE-NEWTON <93740768+1-LEI-DE-NEWTON@users.noreply.github.com>
2022-05-21 15:28:59 -0300

    Add files via upload

---
+++ ordem cres e decres.py
@@ -0,0 +1,17 @@
+
+n1=int(input('informe um numero aleatoriamente' + '\n'))
+n2=int(input('informe um numero aleatoriamente' + '\n'))
+n3=int(input('informe um numero aleatoriamente' + '\n'))
+n4=int(input('informe um numero aleatoriamente' + '\n'))
+n5=int(input('informe um numero aleatoriamente' + '\n'))
+n6=int(input('informe um numero aleatoriamente' + '\n'))
+n7=int(input('informe um numero aleatoriamente' + '\n'))
+n8=int(input('informe um numero aleatoriamente' + '\n'))
+n9=int(input('informe um numero aleatoriamente' + '\n'))
+n10=int(input('informe um numero aleatoriamente' + '\n'))
+
+numeros=[n1,n2,n3,n4,n5,n6,n7,n8,n9,n10]
+print('numeros agrupados em ordem crescente:', sorted(numeros))
+numeros.sort()
+numeros.reverse()
+print('numero agrupados em ordem decrescente:', numeros)
