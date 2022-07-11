ce28a2a02fa7c966022e8454d842b0f5b7d9b71f
1-LEI-DE-NEWTON <93740768+1-LEI-DE-NEWTON@users.noreply.github.com>
2022-07-11 11:37:55 -0300

    Create calculo vetor numeros multiplos de 3.py

---
+++ Vetores/calculo vetor numeros multiplos de 3.py
@@ -0,0 +1,12 @@
+
+vetor_multiplo_de_3 = []
+
+
+def calculo_de_multiplo():
+    for i in range (3,30):
+        if i % 3 == 0:
+            vetor_multiplo_de_3.append(i)
+    print(vetor_multiplo_de_3)
+
+calculo_de_multiplo()
+
